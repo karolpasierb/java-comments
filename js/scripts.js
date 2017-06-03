1e7eaeabb15162c05cb26d301b22424c157a4d37
karol <karolpasierb@op.pl>
2017-06-03 19:39:53 +0200

    task java comments

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+/*
+  Here we will test alert object in Java Scripy
+  We are starting from asking our guest to enter his name
+*/
+var name = prompt('Enter your name');
+// Below we write the text of our alert
+alert('Nice to meet You, ' + name);
+// Below we have our console log
+console.log('Nice to meet You, ' + name);
